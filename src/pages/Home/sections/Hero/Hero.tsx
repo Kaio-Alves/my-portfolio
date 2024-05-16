bf76c36d251ed29feebd397c33c2fd2c3d1608db
unknown <kaiotorres03@gmail.com>
2024-05-16 10:28:32 -0300

    Animated

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -2,32 +2,49 @@
 
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import { Container, Grid, Typography, styled } from "@mui/material";
+import { Box, Container, Grid, Typography, styled } from "@mui/material";
 import Avatar from "../../../../assets/images/perfil.jpg";
+import { AnimatedBackground } from '../../../../components/AnimatedBackground/AnimatedBackground';
 import StyledButton from '../../../../components/StyledButton/StyledButton';
 const Hero = () => {
     const StyledHero = styled("div")(({theme})=> ({
         backgroundColor:theme.palette.primary.main,
-        height:"100vh"
+        height:"100vh",
+        display:"flex",
+        alignItems:"center",
+        [theme.breakpoints.up('xs')]:{
+            paddingTop:"100px"
+        },
+        [theme.breakpoints.up('md')]:{
+            paddingTop:"0"
+        }
     }));
-    const StyledImg = styled("img")(()=> ({
-        width:"100%",
-        borderRadius:"50%"
+    const StyledImg = styled("img")(({theme})=> ({
+        width:"75%",
+        borderRadius:"50%",
+        border:`1px solid ${theme.palette.primary.contrastText}`
     }));
     return (
       <>
         <StyledHero>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={4}>
-                        <StyledImg src={Avatar}/>
+                    <Grid item xs={12} md={5}>
+                        <Box position="relative">
+                            <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                <AnimatedBackground/>
+                            </Box>
+                            <Box position="relative">
+                                <StyledImg src={Avatar}/>
+                            </Box>
+                        </Box>
                     </Grid>
-                    <Grid item xs={12} md={8}>
-                        <Typography color="primary.contrastText" variant="h1" textAlign="center">Kaio Alves</Typography>
+                    <Grid item xs={12} md={7}>
+                        <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Kaio Alves</Typography>
                         <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
-                        <Grid container display="flex" justifyContent="center" spacing={3}>
+                        <Grid container display="flex" justifyContent="center" spacing={3} pb={3}>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
+                                <StyledButton onClick={()=> console.log("download")}>
                                     <DownloadIcon/> 
                                      <Typography>
                                         Download CV
@@ -35,12 +52,11 @@ const Hero = () => {
                                 </StyledButton>
                             </Grid>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
+                                <StyledButton onClick={()=> console.log("contact")}>
                                     <MailOutlineIcon/> 
                                     <Typography>
                                         Contact me
                                     </Typography>
-                                    
                                 </StyledButton>
                             </Grid>
                         </Grid>
